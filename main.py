f7741aaa4668e263260b7bdf2907fbf3285b1602
PAILLEX <loic.paillex@sogeti.com>
2017-05-30 16:51:09 +0200

    blabla

--- main.py
+++ main.py
@@ -4,7 +4,4 @@ from api import names
 
 app = application = bottle.default_app()
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    bottle.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    bottle.run(host='localhost', port=8080, debug=True)
+bottle.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
